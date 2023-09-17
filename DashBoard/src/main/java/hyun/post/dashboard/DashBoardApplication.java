7157ba43447071a82482a3b1f5923ab293e4e703
mothership2002 <mothership2002@naver.com>
2023-09-17 18:21:24 +0900

    no message

---
+++ DashBoard/src/main/java/hyun/post/dashboard/DashBoardApplication.java
@@ -0,0 +1,13 @@
+package hyun.post.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashBoardApplication.class, args);
+    }
+
+}
---
+++ DashBoard/src/test/java/hyun/post/dashboard/DashBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package hyun.post.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
