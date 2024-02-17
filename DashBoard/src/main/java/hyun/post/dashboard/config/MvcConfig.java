8caf1fb3ea15e8300dc4b1d07bc5e0726a5d2d15
LAPTOP-8I3EL5BD\lenovo <mothership2002@naver.com>
2024-02-17 16:47:51 +0900

    원격 시험 완료

--- DashBoard/src/main/java/hyun/post/dashboard/config/MvcConfig.java
+++ DashBoard/src/main/java/hyun/post/dashboard/config/MvcConfig.java
@@ -1,9 +1,20 @@
 package hyun.post.dashboard.config;
 
+import hyun.post.dashboard.interceptor.TaskTimeInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
 
+    private final TaskTimeInterceptor taskTimeInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(taskTimeInterceptor)
+                .addPathPatterns("/**");
+    }
 }
